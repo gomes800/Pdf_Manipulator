e3136a71ef26677c260d9380acc0b92c62d8c1b6
barox <joaogomesleite800@gmail.com>
2025-04-26 11:00:09 -0300

    First commit

---
+++ src/main/java/com/gom/pdf/manipulator/PdfManipulatorApplication.java
@@ -0,0 +1,13 @@
+package com.gom.pdf.manipulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfManipulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfManipulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gom/pdf/manipulator/PdfManipulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.pdf.manipulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfManipulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
